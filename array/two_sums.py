54cc72616a9976d11a6f4323548abff4f7fe7c66
gattadireena <gattadi.reenarani@gmail.com>
2025-12-08 21:31:52 +0530

    two sums

---
+++ array/two_sums.py
@@ -0,0 +1,60 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+
+#Brute Force O(n)^2
+def twoSumBruteForce(array, S):
+    for i in range(len(array)-1):
+        for j in range(i+1,len(array)):
+            if (array[i] + array[j] == S):
+                return [array[i],array[j]]
+    return []
+
+#Data Structure (Dictionary) O(N)
+def twoSumHashing(array, S):
+    hashtable = {}
+    
+    for x in array:
+        y = S-x
+        if (y in hashtable):
+            return [x,y]
+        else:
+            hashtable[x] = True
+    return []
+
+#Data Structure (Set) O(N)
+def twoSumSet(array, S):
+    store = set()
+    for x in array:
+        y = S - x
+        if y in store:
+            return [x,y]
+        else:
+            store.add(x)
+    return []
+
+if __name__ == "__main__":
+    array = [3,4,5,-6,12,11,10,14]
+    S = 5
+    
+    print(twoSumBruteForce(array,S))
+    print(twoSumHashing(array,S))
+    print(twoSumSet(array,S))
+ 
