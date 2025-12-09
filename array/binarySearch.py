2e189dd1ae00f3fae67979ecf081a895116a1ba0
gattadireena <gattadi.reenarani@gmail.com>
2025-12-09 20:25:14 +0530

    binary search

---
+++ array/binarySearch.py
@@ -0,0 +1,17 @@
+def bsearch(arr, target):
+    left = 0
+    right = len(arr)-1
+    while (left<=right):
+        mid = (left+right)//2
+        if(target == arr[mid]):
+            return mid
+        elif(target<arr[mid]):
+            right = mid-1
+        else:
+            left = mid + 1
+    return -1
+
+if __name__ == "__main__":
+    arr = [-1,0,3,5,9,12]
+    target = 90
+    print(bsearch(arr,target))
