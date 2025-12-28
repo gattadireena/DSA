30ace4b00a3412150a10639b89815d11c52023dc
gattadireena <gattadi.reenarani@gmail.com>
2025-12-28 13:53:51 +0530

    majority element n/2 using optiomal method

--- array/majorityElement1.py
+++ array/majorityElement1.py
@@ -34,9 +34,26 @@ def majorityBrute(arr):
         if(cnt > n//2): return arr[i]
     return -1
 
-
+def majorityOptimal(arr):
+    n = len(arr)
+    cnt = 0
+    for i in range(n):
+        if(cnt == 0):
+            cnt = 1
+            el = arr[i]
+        elif (arr[i]==el):
+            cnt += 1
+        else:
+            cnt -= 1
+    cnt1 = 0
+    for i in range(n):
+        if(arr[i] == el): cnt1 += 1
+    if(cnt1 > n/2):
+        return el
+    return -1
 
  
 if __name__ == "__main__":
     arr = [2,2,1,1,1,2,2]
     print(majorityBrute(arr))
+    print(majorityOptimal(arr))
